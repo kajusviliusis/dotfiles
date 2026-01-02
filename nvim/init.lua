20f17412291eec8864520260f84f67d8c360f7d7
kajusviliusis <kajus.viliusis0@gmail.com>
2026-01-02 16:59:02 +0200

    ctrl + arrows works

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
--- automatically load directory in terminal
-vim.opt.autochdir = true
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
@@ -29,23 +28,6 @@ require("lazy").setup({
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
-
--- make sure cmp is installed
-local cmp_status, cmp = pcall(require, "cmp")
-if not cmp_status then
-  return
-end
-
-cmp.setup({
-  mapping = {
-    ["<Up>"] = cmp.mapping.select_prev_item(),
-    ["<Down>"] = cmp.mapping.select_next_item(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Enter to confirm
-    -- keep Tab navigation too
-    ["<Tab>"] = cmp.mapping.select_next_item(),
-    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-  },
-})
 
 require "options"
 require "autocmds"
