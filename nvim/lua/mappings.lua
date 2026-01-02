02b21a75f208d08697c567fb9f21dcd9b9636162
kajusviliusis <kajus.viliusis0@gmail.com>
2026-01-02 17:35:59 +0200

    reset default mapping

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,21 +1,6 @@
 require "nvchad.mappings"
 
 -- add yours here
-local M = {}
-
-M.general = {
-  n = {
-    ["<C-Left>"]  = { "b", "Previous word" },
-    ["<C-Right>"] = { "w", "Next word" },
-  },
-
-  i = {
-    ["<C-Left>"]  = { "<C-o>b", "Previous word" },
-    ["<C-Right>"] = { "<C-o>w", "Next word" },
-  },
-}
-
-return M
 
 
 local map = vim.keymap.set
